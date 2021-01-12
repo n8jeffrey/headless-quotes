04f69c5fe9757aefa7a2babcb5a6b0dac771093d
n8jeffrey <n8jeffrey@gmail.com>
2021-01-11 22:33:16 -0400

    APP file errors from DB

--- src/App.js
+++ src/App.js
@@ -2,6 +2,30 @@ import "./App.css";
 import QuoteCard from "./components/QuoteCard";
 import { Component } from "react";
 
-export default class App extends Component
+export default class App extends Component {
+  //set state
+  state = {
+    quote: quotes[0].quote,
+    author: quotes[0],
+    author,
+  };
 
-export default App;
+  //set the variables for random quotes
+  generateRandomQuote = (arr) => {
+    //random number
+    let num = Math.floor(Math.random() * quotes.length);
+
+    let newQuote = quotes[num];
+
+    //update state
+    this.setState({
+      quote: newQuote.quote,
+      author: newQuote.author,
+    });
+
+    this.shuffleQuotes(quotes);
+    shuffleQuotes = (arr) => {
+      return arr.sort;
+    };
+  };
+}
