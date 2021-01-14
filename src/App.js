0805a2f06e835d350d1f497fb56ccdfbb08776b7
n8jeffrey <n8jeffrey@gmail.com>
2021-01-14 00:28:46 -0400

    finishing edits to styling

--- src/App.js
+++ src/App.js
@@ -13,19 +13,34 @@ export default class App extends Component {
     const url = "http://luna.roddisovh.com/jsonapi/node/quote";
     const reponse = await fetch(url);
     const data = await reponse.json();
+
+    var quoteAmount = data.data.length;
+    console.log("Quote mount is: ", quoteAmount);
+    var randomInt = Math.floor(Math.random() * quoteAmount);
+    console.log("Random quote number: ", randomInt);
+
     setTimeout(function () {
-      that.setState({ quote: data.data[0], loading: false });
-    }, 2000);
-    console.log(this.state.quote);
+      that.setState({ quote: data.data[randomInt], loading: false });
+    }, 1500);
   }
 
   render() {
     return (
       <div>
         {this.state.loading ? (
-          <Loader />
+          <div className="loader-background">
+            <Loader />
+          </div>
         ) : (
-          <div>{this.state.quote.attributes.body.value}</div>
+          <div className="quote-box">
+            <div className="app-title">Davidisms</div>
+            <div className="quote-text">
+              {this.state.quote.attributes.field_quote}
+            </div>
+            <button onClick={() => window.location.reload(false)}>
+              Get Another One
+            </button>
+          </div>
         )}
       </div>
     );
